187195a1a3d00f3cc3cf5b01c345112fbc1ebded
JRCEGA <robertoce1030@gmail.com>
2025-06-02 02:47:04 -0700

    Favicon Text Fix + Speed Metrics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { Poppins } from 'next/font/google'
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const poppins = Poppins({
   subsets: ['latin'],
